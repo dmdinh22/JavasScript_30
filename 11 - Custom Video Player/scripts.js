de8f80f5a419a02b59004c24991b7c70397d823b
David Dinh <dmdinh22@gmail.com>
2018-06-03 00:16:29 -0700

    day11 completed

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,4 +1,4 @@
-// get elements
+// elements
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
@@ -16,16 +16,41 @@ function togglePlay() {
 function updatePlayButton() {
     const icon = this.paused ? '►' : '❚ ❚';
     toggle.textContent = icon;
-}
+} //updatePlayButton
 
 function skip() {
     video.currentTime += parseInt(this.dataset.skip);
-}
+} //skip
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+} //handleRangeUpdate
+
+function handleProgress() {
+    const percent = video.currentTime / video.duration * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+} //handleProgress
+
+function scrub(e) {
+    const scrubTime = e.offsetX / progress.offsetWidth * video.duration;
+    video.currentTime = scrubTime;
+} //scrub
 
 // event listeneers
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updatePlayButton);
 video.addEventListener('pause', updatePlayButton);
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+// checks if mousedown is true first and then runs scrub(e). does not run if !mousedown
+progress.addEventListener('mousemove', e => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => (mousedown = true));
+progress.addEventListener('mouseup', () => (mousedown = false));
