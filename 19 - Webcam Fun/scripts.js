ea29f7fdab0118b2746982608d8fab90f097df4f
David Dinh <dmdinh22@gmail.com>
2018-06-13 00:27:01 -0700

    rgb effects

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -25,6 +25,17 @@ function paintToCanvas() {
 
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
+        // take the pixels out
+        let pixels = ctx.getImageData(0, 0, width, height);
+        // alter them
+        // pixels = redEffect(pixels);
+
+        pixels = rgbSplit(pixels);
+        // ctx.globalAlpha = 0.8;
+
+        // pixels = greenScreen(pixels);
+        // put them back
+        ctx.putImageData(pixels, 0, 0);
     }, 16);
 }
 
@@ -41,6 +52,24 @@ function takePhoto() {
     link.setAttribute('download', 'awesome');
     link.innerHTML = `<img src="${data}" alt="Awesome selfie" />`;
     strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i + 0]; // RED
+        pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+        pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+    }
+    return pixels;
 }
 
 getVideo();
