4e2fd950970c40a076b39e3e62d4d56c744c6a6a
David Dinh <dmdinh22@gmail.com>
2018-07-04 23:24:21 -0700

    display to DOM

--- 29 - Countdown Timer/scripts.js
+++ 29 - Countdown Timer/scripts.js
@@ -1,9 +1,11 @@
 let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
 
 function timer(seconds) {
     const now = Date.now();
-
     const then = now + seconds * 1000;
+    //immediately run it
+    displayTimeLeft(seconds);
 
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -11,8 +13,17 @@ function timer(seconds) {
         if (secondsLeft < 0) {
             return;
         }
-        //display it
 
-        console.log(secondsLeft);
+        //display it - run function again
+        displayTimeLeft(secondsLeft);
     }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${
+        remainderSeconds < 10 ? '0' : ''
+    }${remainderSeconds}`;
+    timerDisplay.textContent = display;
 }
