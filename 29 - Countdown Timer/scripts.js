5727536c865e50e904d8a9639829b88383474180
David Dinh <dmdinh22@gmail.com>
2018-07-05 00:01:11 -0700

    display end time on DOM

--- 29 - Countdown Timer/scripts.js
+++ 29 - Countdown Timer/scripts.js
@@ -1,11 +1,14 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
     //immediately run it
     displayTimeLeft(seconds);
+    // show end time on DOM
+    displayEndTime(then);
 
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -25,5 +28,17 @@ function displayTimeLeft(seconds) {
     const display = `${minutes}:${
         remainderSeconds < 10 ? '0' : ''
     }${remainderSeconds}`;
+    document.title = display; // set tab title to timer
     timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+
+    endTime.textContent = `Be back At ${adjustedHour}:${
+        minutes < 10 ? '0' : ''
+    }${minutes}`;
 }
